19867243f6c53e32ea2091c2b63363eecbe94900
Donvickins <ogavicky@gmail.com>
2025-07-01 08:35:11 +0100

    Optimization: Separation of Concerns

--- cpp_module/agent_screenshot.cpp
+++ cpp_module/agent_screenshot.cpp
@@ -1,9 +1,20 @@
 #include "dxdiag.hpp"
+#include "yolo.hpp"
+#include "utils.hpp"
 
 int main()
 {
+    if (!setUpEnv())
+    {
+        LOG_ERR("Failed to set up environment. Exiting.");
+        return -1;
+    }
+
+    cv::ocl::setUseOpenCL(true);
     DXGIContext ctx;
+    HARDWARE_INFO hw_info;
     cv::Mat screenshot;
+    detectSystemArch(hw_info);
     // Initialize COM for WIC (Windows Imaging Component)
     // CoInitializeEx is needed for COM operations like CoCreateInstance.
     HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
@@ -22,6 +33,20 @@ int main()
     std::cout << "Starting screenshot capture loop. Press Ctrl+C to stop." << std::endl;
 
     const std::chrono::seconds interval(5); // Capture every 5 seconds
+
+    // Now initialize YOLO
+    cv::dnn::Net yolo_net;
+    std::vector<std::string> class_names_vec;
+
+    const std::string YOLO_MODEL_PATH = (std::filesystem::current_path() / "models/yolo/yolo11l.onnx").generic_string();
+    const std::string CLASS_NAMES_PATH = (std::filesystem::current_path() / "models/yolo/coco.names.txt").generic_string();
+
+    LOG("Initializing YOLO network...");
+    if (!setupYoloNetwork(yolo_net, YOLO_MODEL_PATH, CLASS_NAMES_PATH, class_names_vec, hw_info))
+    {
+        LOG_ERR("Failed to setup YOLO network");
+        return -1;
+    }
     // Main loop for capturing screenshots
     while (true)
     {
@@ -37,12 +62,12 @@ int main()
                 if (hr == DXGI_ERROR_DEVICE_REMOVED || hr == DXGI_ERROR_DEVICE_RESET || hr == DXGI_ERROR_DEVICE_HUNG)
                 {
                     std::cerr << "DirectX device lost or reset. Attempting to reinitialize..." << std::endl;
-                    Cleanup(ctx);                     // Clean up existing components
-                    hr = InitDesktopDuplication(ctx); // Try to reinitialize
+                    Cleanup(ctx);
+                    hr = InitDesktopDuplication(ctx);
                     if (FAILED(hr))
                     {
                         std::cerr << "Failed to reinitialize DirectX. Exiting." << std::endl;
-                        break; // Exit loop on critical failure
+                        break;
                     }
                 }
                 else if (hr == DXGI_ERROR_ACCESS_LOST)
@@ -53,7 +78,7 @@ int main()
                         ctx.pDesktopDupl->Release();
                         ctx.pDesktopDupl = nullptr;
                     }
-                    hr = InitDesktopDuplication(ctx); // Try to reinitialize (will create new g_DeskDupl)
+                    hr = InitDesktopDuplication(ctx);
                     if (FAILED(hr))
                     {
                         std::cerr << "Failed to re-establish desktop duplication. Exiting." << std::endl;
@@ -67,8 +92,26 @@ int main()
             }
 
             // Implement yoloProcessing here
+            // try
+            // {
+            //     processFrameWithYOLO(screenshot, yolo_net, class_names_vec);
+            // }
+            // catch (const cv::Exception &e)
+            // {
+            //     LOG_ERR("OpenCV error processing YOLO: " << e.what());
+            //     continue;
+            // }
+            // catch (const std::exception &e)
+            // {
+            //     LOG_ERR("Processing frame YOLO: " << e.what());
+            //     break;
+            // }
 
-            // Wait for the specified interval before the next capture
+            // cv::imshow("Screenshot", screenshot);
+            // cv::waitKey(1);
+            // if (cv::getWindowProperty("Screenshot", cv::WND_PROP_VISIBLE) < 1)
+            //     break;
+
             std::this_thread::sleep_for(interval);
         }
         catch (const std::exception &e)
